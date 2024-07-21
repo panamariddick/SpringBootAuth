b3cb7f0a4fbc3ada6a1d4f07223ec2474d7b2426
rgonzalez <rgonzalez@tropigas.com.pa>
2024-07-21 13:10:20 -0500

    Segundo commit

---
+++ AuthService/src/main/java/com/example/authservice/model/User.java
@@ -0,0 +1,53 @@
+package com.example.authservice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users", indexes = {
+        @Index(name = "idx_username", columnList = "username", unique = true)
+})
+public class User {
+    @Id
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
